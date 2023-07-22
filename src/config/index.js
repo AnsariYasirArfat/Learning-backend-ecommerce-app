ae56a61d00159a6884f06e3e491495e5fb942ae7
YASEER ARFAT ANSARI <119731482+AnsariYasirArfat@users.noreply.github.com>
2023-07-22 08:43:55 +0000

    forgot password functionality added

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv"
 dotenv.config()
 const config = {
     PORT: process.env.PORT || 5000,
-    MONGOBD_URL: process.env.MONGOBD_URL || "mongodb://localhost:27017/ecommerce"
+    MONGOBD_URL: process.env.MONGOBD_URL || "mongodb://localhost:27017/ecommerce",
+    JWT_SECRET: process.env.JWT_SECRET || "yoursecret",
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "30d"
 }
 
 export default config
