e58d9e594626a20b419a2d897600df671bebb8ab
YASEER ARFAT ANSARI <119731482+AnsariYasirArfat@users.noreply.github.com>
2023-07-23 05:40:06 +0000

    user logged In & Out controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -12,6 +12,13 @@ export const cookieOptions = {
     httpOnly: true
 }
 
+/******************************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @returns User Object
+ ******************************************************/
+
 export const signUp = asyncHandler(async (req, res) => {
     // Get data from user
     const { name, email, password } = req.body
@@ -49,3 +56,55 @@ export const signUp = asyncHandler(async (req, res) => {
         user,
     })
 })
+
+/*********************************************************
+ * @LOGIN
+ * @route http://localhost:5000/api/auth/login
+ * @description User Login Controller for signing in the user
+ * @returns User Object
+ *********************************************************/
+
+export const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    // Validation
+    if (!email || !password) {
+        throw new CustomError("Please fill all details", 400)
+    }
+    const user = User.findOne({ email }).select("+password")
+    if (!user) {
+        throw new CustomError("Invalid creadentials", 400)
+    }
+    const isPasswordMatched = await user.comparePassword(password)
+    if (isPasswordMatched) {
+        const token = user.getJWTtoken()
+    user.password = undefined
+    res.cookie("token", token, cookieOptions)
+    return res.status(200).json({
+        success: true,
+        token,
+        user
+    })
+    }
+    throw new CustomError("password is incorrect", 400)
+})
+
+/**********************************************************
+ * @LOGOUT
+ * @route http://localhost:5000/api/auth/logout
+ * @description User Logout Controller for logging out the user
+ * @description Removes token from cookies
+ * @returns Success Message with "Logged Out"
+ **********************************************************/
+
+export const logout = asyncHandler( async (req, res )=>{
+    res.cookie("token", null , {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
+})
+
