c1a7cd3cfab8feb5a938199af72c15a440cd45c3
YASEER ARFAT ANSARI <119731482+AnsariYasirArfat@users.noreply.github.com>
2023-07-24 07:29:18 +0000

    image uload to s3

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,11 @@ const config = {
     PORT: process.env.PORT || 5000,
     MONGOBD_URL: process.env.MONGOBD_URL || "mongodb://localhost:27017/ecommerce",
     JWT_SECRET: process.env.JWT_SECRET || "yoursecret",
-    JWT_EXPIRY: process.env.JWT_EXPIRY || "30d"
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    S3_REGION: process.env.S3_REGION,
 }
 
 export default config
