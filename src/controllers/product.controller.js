9622d6b57fab141560eae3ff63629fc572848e80
YASEER ARFAT ANSARI <119731482+AnsariYasirArfat@users.noreply.github.com>
2023-07-24 11:34:07 +0000

    how to upload files using formidable

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,29 @@
+import Products from "../Models/product.schemajs"
+import formidable from "formidable"
+import { s3FileUpload, s3FileUpload } from '../service/imageUpload.js'
+import mongoose from "mongoose"
+import asyncHandler from "../service/asyncHandler.js"
+import CustomError from "../utils/CustomError.js"
+import config from "../config/index.js"
+
+/**********************************************************
+ * @ADD_PRODUCT
+ * @route https://localhost:5000/api/product
+ * @description Controller used for creating a new product
+ * @description Only admin can create the coupon
+ * @descriptio Uses AWS S3 Bucket for image upload
+ * @returns Product Object
+ *********************************************************/
+
+export const addProduct = asyncHandler(async (req, res) => {
+    const form = formidable({ multiples: true, keepExtensions: true });
+    form.parse(req, async function (err, fields, files) {
+        if (err) {
+            throw new CustomError(err.message || "Something went wrong", 500)
+        }
+
+        let productId = new Mongoose.Types.ObjectId().toHexString()
+
+        console.log(fields, files);
+    })
+})
